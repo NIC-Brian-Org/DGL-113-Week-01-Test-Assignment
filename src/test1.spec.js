9f3b5621847af22beff510ac9121313cc9b112ea
nic-brian <86538642+nic-brian@users.noreply.github.com>
2023-01-06 16:46:52 -0800

    split up test cases

---
+++ src/test1.spec.js
@@ -0,0 +1,26 @@
+import exp from "constants";
+import path from "path";
+
+describe("variables", () => {
+  const dialogHandler2 = jest.fn(dialog => dialog.dismiss());
+
+  beforeAll(async () => {
+    const URL = `file:///${path.resolve(__dirname, "./index.html")}`;
+    page.on("dialog", dialogHandler2);
+    await page.goto(URL, {
+      waitUntil: "networkidle2"
+    });
+  });
+
+  it("proPrice == 99", async () => {
+    const proPrice = await page.evaluate("proPrice");
+    expect(proPrice).toEqual(99);
+  });
+
+  it("enterprisePrice == 198", async () => {
+    const enterprisePrice = await page.evaluate("enterprisePrice");
+    expect(enterprisePrice).toEqual(198);
+  });
+  
+
+});
